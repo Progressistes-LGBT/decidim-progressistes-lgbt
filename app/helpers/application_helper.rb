2beb0dba0e17eca33133a5404c853cac46a6bae9
Marine Boudeau <m14868@gmail.com>
2019-05-30 09:50:06 +0200

    Test modify with helsinki's theme

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,26 @@
 module ApplicationHelper
+  # include Decidim::Plans::LinksHelper
+
+  # Defines whether the "common" content elements are displayed. In the
+  # 'private' application mode these should be hidden in case the user is not
+  # signed in.
+  def display_common_elements?
+    if is_private_mode?
+      return user_signed_in?
+    end
+    true
+  end
+
+  def is_private_mode?
+    Rails.application.config.use_mode == 'private'
+  end
+
+  def feedback_email
+    Rails.application.config.feedback_email
+  end
+
+  def meta_image_default
+    return "test-social.jpg"
+  end
+
 end
--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,25 @@ module DecidimProgressistesLgbt
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    # String identifier, this defines the main mode of Decidim
+    # See README.md for more explation on this.
+    #
+    # Available throughout code as: Rails.application.config.use_mode
+    # Allowed values are:
+    #   'private' : string = Private mode (force logging in)
+    #   'normal' : string = Normal mode without modifications
+    config.use_mode = 'normal'
+
+    # Wrapper class can be used to customize the coloring of the platform per
+    # environment. This is used mainly for the Ideapaahtimo/KuVa instance.
+    config.wrapper_class = 'wrapper-default'
+
+    # Color profile that changes the logo color for header and footer
+    config.color_profile = 'black'
+
+    # The feedback email in the footer of the site
+    config.feedback_email = 'omastadi@hel.fi'
+
   end
 end
